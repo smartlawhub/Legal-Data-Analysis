a7633f20ac3f689aea837a6a48ca1351f3494796
Damien Charlotin <damien.charlotin@sciencespo.fr>
2023-03-17 19:27:08 +0100

    Update Code Mighty Python v2.py

--- Final Presentations/Code/Code Mighty Python v2.py
+++ Final Presentations/Code/Code Mighty Python v2.py
@@ -25,3 +25,25 @@ driver.find_element(By.XPATH, “/html/body/div[2]/div/div/form/div[1]/div[1]/di
 driver.find_element(By.XPATH, “/html/body/div[2]/div/div/form/div[2]/input[1]”).click()  # Click on the “Search” button at the bottom of the page
 
 driver.find_element(By.XPATH, “/html/body/div[2]/div/div/div/table/tbody/tr[2]/td[2]/a”).click()
+
+# To start
+
+soup = BeautifulSoup(driver.page_source) # Correspondra à la landing page
+lis = soup.find_all("li", id=re.compile("^fancytree-"))
+DFS = []
+for li in lis:
+    url = "https://www.wipo.int/amc/en/domains/search/legalindex/results.jsp?ids1=" + li.get("id").split("-")[-1]
+    df = pd.read_html(url)[0]
+    driver.get(url)
+    subsoup = BeautifulSoup(driver.page_source)
+    aas = subsoup.find_all("a", text=re.compile("[A-Z]+\d+-\d+"))
+    dd = {}
+    for a in aas:
+        dd[a.text] = a.get("href")
+    df["URL"] = df["Case Details"].map(dd)
+    for a in aas:
+        driver.get(a.get("href"))
+        # Add code to add those elements to the dataframe
+    DFS.append(df)
+    
+df = pd.concat(df)
